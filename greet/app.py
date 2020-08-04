4fb4f83b4c68ba13ed69ce936d6ff3730588296f
Jinal Shah <jinal.shah.design@gmail.com>
2020-08-03 22:43:09 -0700

    Exercise Completed

--- greet/app.py
+++ greet/app.py
@@ -1,17 +1,21 @@
+from flask import Flask
+
+app = Flask(__name__)
+
 @app.route('/welcome')
-def say_hello():
+def say_welcome():
     """Return welcome at this route"""
-    html = "<html><body><h1>welcome</h1></body></html>"
+    html = "<html><body><h1>Welcome</h1></body></html>"
     return html
 
 @app.route('/welcome/home')
-def say_hello():
+def say_welcome_home():
     """Return welcome home"""
-    html = "<html><body><h1>welcome home</h1></body></html>"
+    html = "<html><body><h1>Welcome Home</h1></body></html>"
     return html
 
 @app.route('/welcome/back')
-def say_hello():
+def say_welcome_back():
     """Return welcome back"""
-    html = "<html><body><h1>welcome back</h1></body></html>"
+    html = "<html><body><h1>Welcome Back</h1></body></html>"
     return html
